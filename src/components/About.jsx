27b0e34c2b72a5b9257e769f57932eefdcc7cc20
laura <vongsy.laura@gmail.com>
2023-01-11 15:01:43 +0100

    premier commit

---
+++ src/components/About.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div
+            name="about"
+            className="w-full h-screen bg-[#0a192f] text-gray-300"
+        >
+            <div className="flex flex-col justify-center items-center w-full h-full">
+                <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+                    <div className="sm:text-right pb-8 pl-4">
+                        <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+                            About
+                        </p>
+                    </div>
+                    <div></div>
+                </div>
+                <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+                    <div className="sm:text-right text-4xl font-bold">
+                        <p>
+                            Hi. I'm Laura Vongsy, nice to meet you. Please take
+                            a look around.
+                        </p>
+                    </div>
+                    <div>
+                        <div>
+                            I had been working as a nurse for a few years before
+                            I felt the urge to ask myself questions about what I
+                            really wanted to do for the rest of my life.
+                            <br></br> I began to make researches to find the job
+                            that would suit me the best, and this is how I got
+                            interested in Web Development. <br></br> I
+                            discovered a whole new world, full of challenges and
+                            full of opportunities, and I was hooked!<br></br>{" "}
+                            Soon after, I subscribed to an online Web
+                            Development class with Openclassrooms, and 6 month
+                            later I graduated!<br></br>I rapidly acquired skills
+                            in HTML, CSS, Sass and Javascript. I then worked on
+                            APIs and I learned to code with, Node Js, Express
+                            and I learned to use MongoDb and Firebase.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Booki from "../assets/Booki.png";
+import Ohmyfood from "../assets/Ohmyfood.jpg";
+import Panthere from "../assets/Panthere.png";
+import kanap from "../assets/KANAP.png";
+import Piiquante from "../assets/piiquante.png";
+import groupomania from "../assets/groupomania.png";
+
+export const data = [
+    {
+        id: 1,
+        name: "Réalisation du CSS d'un site de réservation d'hôtel",
+        image: Booki,
+        github: "https://github.com/LauraVongsy/booki.git",
+        live: "lv-booki.netlify.app",
+    },
+    {
+        id: 2,
+        name: "Réalisation d'un site de menus gastronomiques",
+        image: Ohmyfood,
+        github: "https://github.com/LauraVongsy/ohmyfood.git",
+        live: "https://lv-ohmyfood.netlify.app",
+    },
+    {
+        id: 3,
+        name: "Amélioration du SEO d'un site vitrine",
+        image: Panthere,
+        github: "https://github.com/LauraVongsy/panthere.git",
+        live: "",
+    },
+    {
+        id: 4,
+        name: "Réalisation d'une API pour un site de e-commerce",
+        image: kanap,
+        github: "https://github.com/LauraVongsy/kanap-backend.git",
+        live: "https://lv-kanap.netlify.app",
+    },
+    {
+        id: 5,
+        name: "Réalisation d'une API pour un site d'avis gastronomiques",
+        image: Piiquante,
+        github: "https://github.com/LauraVongsy/piiquante-backend.git",
+        live: "https://lv-piiquante.netlify.app",
+    },
+    {
+        id: 6,
+        name: "Réalisation d'un réseau social d'entreprise",
+        image: groupomania,
+        github: "https://github.com/LauraVongsy/groupomania-backend.git",
+        live: "https://lv-groupomania.netlify.app",
+    },
+];
