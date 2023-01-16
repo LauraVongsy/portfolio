1e93f99367539145b9db5d1c490f4eda51688b28
laura <vongsy.laura@gmail.com>
2023-01-16 20:23:01 +0100

    fix: responsive design

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,12 +11,9 @@ import fireBase from "../assets/skills/firebase.png";
 
 const Skills = () => {
     return (
-        <div
-            name="skills"
-            className="w-full h-screen bg-[#0a192f] text-gray-300"
-        >
+        <div name="skills" className="w-full  bg-[#0a192f] text-gray-300">
             {/* Container */}
-            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full py-20">
                 <div>
                     <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
                         Skills
