6a92579fd45452b4c650571cde0098f51ca6d27a
LauraVongsy <120934331+LauraVongsy@users.noreply.github.com>
2023-09-07 16:43:49 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -126,7 +126,7 @@ const Navbar = () => {
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
                         <a
                             className="flex justify-between items-center w-full text-gray-300"
-                            href="https://www.linkedin.com/in/laura-vongsy-dev-web/"
+                            href="https://www.linkedin.com/in/laura-vongsy-web-dev/"
                             target="_blank"
                             rel="noreferrer"
                         >
